c2460187a8210bf7b3bfa7752e0bac49758f937c
warrioryoko <miangpellegri@gmail.com>
2019-05-08 10:36:31 -0700

    Refactored to use JSX instead of core React calls to create elements

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
-// const Pet = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props.name),
-//     React.createElement("h2", {}, props.animal),
-//     React.createElement("h2", {}, props.breed)
-//   ]);
-// };
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", { id: "something-important" }, [
@@ -31,4 +19,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
