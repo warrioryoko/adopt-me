f90a9dcf17e9df3899826a7b3b239357404ec680
warrioryoko <miangpellegri@gmail.com>
2019-05-08 15:11:12 -0700

    Used 'Parcel' build tool to install ANIMALS dependency from @frontendmasters. Added select breeds dropdown, with .maps for animal breed. Selectable breeds not yet implemented.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
+// ANIMALS is a list of strings. ANIMALS.map(animal => (<option></option>))
+// is generating individual select/dropdown selections with individual animals
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+  // breeds has an initial state set to an empty array. Likewise, breed is set to an empty string
+  // breed will be filled with whatever option the user chooses after selecting an animal.
+  // breeds will be filled with whatever sub-breeds of the animal selected by the user.
+  // breeds will request against the API - "I have an {animal}, give me all of the {animal.breeds}"
 
   return (
     <div className="search-params">
@@ -17,7 +27,41 @@ const SearchParams = () => {
             onChange={e => setLocation(e.target.value)}
             // Any time there is a change within the input, an event "e" occurs,
             // calls "setLocation" and changes the input value to whatever the user types in
+            // Using implicit return for this arrow function, with the parenthesis
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={breeds.legnth === 0}
+          >
+            <option>All</option>
+            {breeds.map(breedString => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
