cd1570aebd2647ceeb81f31600f32363431cbc48
warrioryoko <miangpellegri@gmail.com>
2019-05-12 19:37:57 -0700

    Created carousel component, that handles switching between images within the Details component.

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 // <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
 // This will show me everything Reach Router is passing in
@@ -42,10 +43,11 @@ class Details extends React.Component {
       return <h1>loading...</h1>;
     }
 
-    const { animal, breed, location, description, name } = this.state;
+    const { animal, breed, location, description, name, media } = this.state;
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
